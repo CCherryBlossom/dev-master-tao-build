fa5499df88e523e5b7a67b4c99631fc9521e90e9
Alevtyna Shelkovnikova <158612382+CCherryBlossom@users.noreply.github.com>
2024-02-18 14:15:54 +0200

    project completed
    project completed

---
+++ assets/js/custom.js
@@ -0,0 +1,131 @@
+$(document).ready(function(){
+  // Burger-menu
+  const burgers = document.querySelectorAll('.hamburger')
+  const nav = document.querySelector('.header__nav')
+
+  if (burgers.length > 0) {
+    burgers.forEach(element => {
+      element.addEventListener('click', function(){
+        element.classList.toggle('is-active');
+        nav.classList.toggle('show');
+      })
+    })
+  }
+
+  // Banner-Slider
+  // setting-name: setting-value - Подключение функций 
+  $('#banner').slick({
+      arrows: true,
+      dots: true,
+      appendArrows: $('.banner-arrows'),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            arrows: false,
+          }
+        },
+      ]
+    });
+    
+  
+    // File-Input
+    const fileInput = document.querySelector('#fileInput');
+    const fileName  = document.querySelector('#file-name')
+  
+    fileInput.addEventListener('change', function() {
+      fileName.innerHTML = this.files[0].name
+    });
+  
+    // Move-chekbox "Calculation"
+    const chekboxes = document.querySelectorAll('[type="checkbox"]');
+    if(chekboxes.length > 0) {
+      chekboxes.forEach(chekbox => {
+        chekbox.closest('.calculation-column').classList.add('move-checkbox');
+      })
+    }
+  
+    // Move-code "Calculation"
+    const codes = document.querySelectorAll('#code');
+    if(codes.length > 0) {
+      codes.forEach(code => {
+        code.closest('.calculation-column').classList.add('move-code');
+      })
+    }
+    
+    // Move-input-name "Calculation"
+    const file = document.querySelectorAll('.input-file')
+    if(file.length > 0) {
+      file.forEach(item => {
+        item.closest('.calculation-column').style.paddingTop = '6px'
+      })
+    }
+
+  // Advantages-Slider
+  mobileOnlySlider("#advantages-slider", true, false, 1024);
+
+  function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+    
+    var slider = $($slidername);
+    var settings = {
+      mobileFirst: true,
+      dots: $dots,
+      arrows: $arrows,
+      responsive: [
+        {
+          breakpoint: $breakpoint,
+          settings: "unslick"
+        }
+      ]
+    };
+  
+    slider.slick(settings);
+    
+    $(window).on("resize", function () {
+      if ($(window).width() > $breakpoint) {
+        return;
+      }
+      if (!slider.hasClass("slick-initialized")) {
+        return slider.slick(settings);
+      }
+    });
+  }
+
+  // template-slider
+  $('.template-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+          dots: true,
+        }
+      },
+    ]
+  });
+    
+
+  // Add class .charter
+  function addClassCharter() {
+    const section = document.querySelectorAll('section')
+    section.forEach(item => item.classList.add('charter'))
+  }
+  
+  document.querySelector('.home-page') ? addClassCharter() : null;
+});
